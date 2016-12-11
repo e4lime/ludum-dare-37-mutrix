6168a03f86b6cc666b5fef40d07ab7199b4f9976
Emil Lindberg <emil.lindberg86@gmail.com>
2016-12-11 21:30:34 +0100

    Remove Leantween and add DoTween

---
+++ Assets/Hovering.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Mutrix.WorldObjects {
+    public class Hovering : MonoBehaviour {
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
